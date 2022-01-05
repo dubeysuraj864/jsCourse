ee29c84c9451e356fbcb9653dca3a4c1ba3c5f9f
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-05 21:01:01 +0530

    sdfsf

---
+++ 3.ExpressionsAndOperators/index.js
@@ -0,0 +1,81 @@
+// console.log(5+20); // + is operator // 5 is operand and that all are called expression
+
+//Assignment Operator (=),(==)
+
+// console.log(` Is both are equal or not : ${ x == y}` );
+
+//Arithmetic
+
+// console.log(3+4);
+// console.log(2-4);
+// console.log(2/4);
+// console.log(2*3);
+// console.log(321%10);
+
+// Increment and decrement operator
+// x++ postfix
+// ++x prefix 
+
+// comparison operator
+// var a =4;
+// var b = 4;
+// // console.log( a == b);
+
+// console.log( a != b);
+
+// > <
+// <= >=
+ 
+
+//Logical and (&&) it will give ouput when all the  expression aRE true
+
+
+
+// var a = 23;
+// var b = 223;
+
+// console.log( a < b && b > 200 ) true
+
+// || it will give true when at least one expression is correct
+// console.log( a < b || b > 3434 )
+
+
+//logical NOT !
+
+
+// var a = 434;
+// var b = 34;
+
+// console.log( !(a> b))
+
+
+// string operator (+ concatination)
+
+// console.log("suraj" + "Dubey") 
+
+
+// (3**3) what will be the ans ???? = 27
+// ** mean is to power 3**3 = 3*3*3
+//9**2 = 9*9   its called exponentiation operator
+
+// console.log(10**4)
+
+// var a = 5;
+// var b = 10;
+// console.log(a + " " + b)
+
+// a = a + b; //15
+// b = a - b; // 5
+// a = a - b; // 10
+
+// console.log(a + " " + b)
+
+// var num1 = '5';
+// var num2 = '5';
+
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(num2);
+
+// console.log(num1 == num2) //this check only value is equal to or not
+// console.log(num1 === num2) // and this one check value is equal or not and as well as data type of the value
